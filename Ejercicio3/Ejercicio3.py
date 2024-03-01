b9ae94d22259684ac8f4c1624e2660f0e2aedb09
DANIELSistems <danielvasquez@colegioguanenta.edu.co>
2024-03-01 08:49:24 -0500

    se realizo el ejercicio 3

---
+++ Ejercicio3/Ejercicio3.py
@@ -0,0 +1,25 @@
+#input
+
+precio_costo=int(input("digite el costo del articulo: "))
+
+#processing
+
+if precio_costo<=3000:
+        ganancia=precio_costo*0,15
+
+elif precio_costo<=6000:
+        ganancia=500
+
+else:
+        ganancia= precio_costo*0,25
+
+precio_venta=precio_costo+ganancia
+
+#output
+
+print( " El producto tiene un costo de $" + str(precio_costo) + " con una ganancia de $" + str(ganancia) + " queda con un precio de venta de $" + str(precio_venta))
+
+
+
+
+
---
+++ Ejercicio3/README.md
@@ -0,0 +1,23 @@
+# EJERCICIO NO. 3 ganancia de una papeleria
+
+## ganancia de una papeleria
+
+ # ANALISIS
+
+variable de entrada(input)
+
+ganancias, gastos, costo de la papeleria
+
+variables de proceso y salida(processing, storage, output)
+
+RTA=precio de la venta
+
+RTA=costo del articulo
+
+RTA=ganancias
+
+# DISEÑO
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
+
+# CONSTRUCCION
