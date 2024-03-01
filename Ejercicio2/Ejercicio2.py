5eb8c56b08097631eacce4eec14adb494d892f8a
DANIELSistems <danielvasquez@colegioguanenta.edu.co>
2024-03-01 08:05:10 -0500

    se reaizo el ejercicio 2

---
+++ Ejercicio2/Ejercicio2.py
@@ -0,0 +1,22 @@
+
+#input
+
+
+ingresos=int(input("digite sus ingresos"))
+
+
+#processing
+
+if ingresos<945200:
+    print("No puede hacer el pestamo ya que sus ingresos son menores a $945200")
+
+if ingresos>=945200:
+    deudas=int(input("Tiene alguna deuda:SI TIENE Digite (1),SI NO TIENE Digite (2): "))
+    SN=deudas
+
+if SN==(1):
+    print("No puede hacer el prestamo ya que tiene deudas")
+
+else:
+    SN==(2)
+    print("Puede hacer el prestamo ya que no tiene deudas y sus ingresos son mayores a $945200")
---
+++ Ejercicio2/README.md
@@ -0,0 +1,23 @@
+# EJERCICIO NO. 2 prestamo bancaria 
+
+## prestamo bancario
+
+ # ANALISIS
+
+variable de entrada(input)
+
+ingresos del cliente
+
+variables de proceso y salida(processing, storage, output)
+
+RTA=aceptado el prestamo
+
+RTA=rechazado el prestamo
+
+RTA=rechazado el prestamo porque no tiene deudas
+
+# DISEÑO
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
+
+# CONSTRUCCION
