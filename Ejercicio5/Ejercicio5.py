c9ea768bb63d614b16c4a126ad71fd15c4ed314a
DANIELSistems <danielvasquez@colegioguanenta.edu.co>
2024-03-08 08:38:59 -0500

    se realizo el ejercicio 5

---
+++ Ejercicio5/Ejercicio5.py
@@ -0,0 +1,23 @@
+# input
+
+metros_gastados=int(input("digite los metros cuadrados: "))
+
+#processing
+
+if metros_gastados >50:
+    costo=0
+
+elif metros_gastados>50 and metros_gastados <=200:
+    costo=2000 * (metros_gastados - 50)
+
+else:
+    metros_gastados >200
+    costo=3000 * (metros_gastados - 50)
+
+costo_agua=10000 + algo
+
+#output
+
+print("el costo del agua es: ",costo_agua)
+
+ 
---
+++ Ejercicio5/README.md
@@ -0,0 +1,9 @@
+# TALLER CONDICIONALES No. 5 
+
+## Progama de sacar el precio del agua
+
+# DISEÑO
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
+
+# CONSTRUCCION
